516c272cec584ca822f9eb82217363dcbab936c8
Niklas Beierl <beierl09@ads.uni-passau.de>
2021-05-14 23:25:41 +0200

    Update to encode_snapshot:
    Pickling and measuring time.

--- encode_snapshot.py
+++ encode_snapshot.py
@@ -1,11 +1,18 @@
 import pandas as p
+import pickle
+from time import time
 from encoding.memory_graph import Pointer, build_memory_graph
 from encoding import SandwichEncoder
 
+POINTER_SIZE = 8
 df = p.read_csv("./data_dump/memory_layer_nokaslr_pointers_translated.csv")
 df = df.dropna()
 pointers = [Pointer(*tup) for tup in df[["offset", "physical"]].itertuples(index=False)]
 
-het_g = build_memory_graph(pointers, SandwichEncoder("./data_dump/memory_layer_nokaslr.raw"))
+start = time()
+het_g = build_memory_graph(pointers, SandwichEncoder("./data_dump/memory_layer_nokaslr.raw", POINTER_SIZE))
+print(f"Encoding took {time() - start} seconds")
+with open("./memory_graph.pkl", "wb+") as f:
+    pickle.dump(het_g, f)
 
 print("Done")
