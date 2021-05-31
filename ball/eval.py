be77b3c3fe047909b8ee1cd74390468b2eb34ed0
Niklas Beierl <beierl09@ads.uni-passau.de>
2021-05-31 19:25:16 +0200

    Adding metrics to eval.py

--- ball/eval.py
+++ ball/eval.py
@@ -1,25 +1,43 @@
 import pickle
-import warnings
+import numpy as np
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, f1_score
 from torch.nn.functional import softmax
 
-warnings.filterwarnings("ignore", message="DGLGraph\.__len__")
+import warnings
 
+warnings.filterwarnings("ignore", message="DGLGraph\.__len__")
 
 with open("./ball-mem-graph.pkl", "rb") as f:
     mem_graph = pickle.load(f)
 
-
 with open("./results.pkl", "rb") as f:
     results = pickle.load(f)
-    if results.is_cuda:
-        results = results.cpu()
-    results = softmax(results)
-    results = results.detach().numpy()
 
-yay = results[mem_graph.ndata["pids"] != -1, 1].mean()
-nay = results[mem_graph.ndata["pids"] == -1, 0].mean()
 
-tasks_results = results[mem_graph.ndata["pids"] != -1]
+post_results = softmax(results.cpu(), dim=1)
+post_results = post_results.detach().numpy()
+
+yay = post_results[mem_graph.ndata["pids"] != -1, 1]
+yay_mean = yay.mean()
+yay_std = yay.std()
+nay = post_results[mem_graph.ndata["pids"] == -1, 0].mean()
+nay_mean = nay.mean()
+nay_std = nay.std()
+
+post_results = post_results.argmax(1)
+pids = set(mem_graph.ndata["pids"].numpy())
+task_results = [np.array(post_results[mem_graph.ndata["pids"] == pid]) for pid in sorted(pids) if pid != -1]
+length = max(len(tr) for tr in task_results)
+task_results = [np.resize(tr, length) for tr in task_results]
+task_results = np.stack(task_results)
+
+labels = (mem_graph.ndata["pids"] != -1).numpy()
+
+
+m_confusion = confusion_matrix(labels, post_results, normalize="true")
+m_recall = recall_score(labels, post_results, average="macro")
+m_prec = precision_score(labels, post_results, average="macro")
+m_f1 = f1_score(labels, post_results, average="macro")
 
 # This file only really makes sense if used in a debugger with facilities to properly visualise tensors / arrays.
 print("Done")
