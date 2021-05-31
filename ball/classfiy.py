b83a302f32fe071f9ad773a83a5bf5bec434990d
Niklas Beierl <beierl09@ads.uni-passau.de>
2021-05-31 15:42:13 +0200

    Update: Applying model in batches.
    This allows for a lot more sophisticated models.

--- ball/classfiy.py
+++ ball/classfiy.py
@@ -1,8 +1,15 @@
 import pickle
-from torch.nn.functional import one_hot
+import torch as t
+import numpy as np
+import dgl.dataloading as dgldl
+from sklearn.metrics import confusion_matrix
+from torch.nn.functional import one_hot, softmax
+from hyperparams import BALL_CONV_LAYERS
 import warnings
 
 warnings.filterwarnings("ignore", message="DGLGraph\.__len__")
+
+BATCH_SIZE = 3000
 
 with open("./ball-mem-graph.pkl", "rb") as f:
     mem_graph = pickle.load(f)
@@ -16,7 +23,27 @@ blocks_one_hot = one_hot(mem_graph.ndata["blocks"].long())
 blocks_one_hot = blocks_one_hot.reshape(blocks_one_hot.shape[0], -1)
 mem_graph.ndata["blocks"] = blocks_one_hot.float()
 
-results = model(mem_graph)
+output_size = model.class_layers[-1].out_features
+results = t.full((mem_graph.num_nodes(), output_size), float("inf"), dtype=t.float32)
+sampler = dgldl.MultiLayerFullNeighborSampler(BALL_CONV_LAYERS)
+loader = dgldl.NodeDataLoader(
+    mem_graph,
+    list(range(mem_graph.num_nodes())),
+    sampler,
+    batch_size=BATCH_SIZE,
+)
+
+last_prec_done = 0
+for i, chunk in enumerate(loader):
+    input_nodes, output_nodes, blocks = chunk
+    blocks = [b.to(t.device("cuda")) for b in blocks]
+    batch_result = model.forward_batch(blocks)
+    results[output_nodes, :] = batch_result.detach().cpu()
+    perc = BATCH_SIZE * i * 100 // mem_graph.num_nodes()
+    if perc % 5 == 0 and perc != last_prec_done:
+        print(f"{perc}% done.")
+        last_prec_done = perc
+
 
 with open("./results.pkl", "wb+") as f:
     pickle.dump(results, f)
--- networks/embedding.py
+++ networks/embedding.py
@@ -1,22 +1,43 @@
-from torch import nn
+from torch.nn import ModuleList, Module, Linear
 from torch.nn import functional as F
-import dgl.nn.pytorch as dglnn
+from dgl.nn.pytorch import GraphConv, HeteroGraphConv
+from hyperparams import BALL_CONV_LAYERS
 
 
-class MyConvolution(nn.Module):
+class MyConvolution(Module):
     def __init__(self, graph, in_size, hidden_size, out_size):
         super(MyConvolution, self).__init__()
-        self.layer1 = dglnn.HeteroGraphConv(
-            {etype: dglnn.GraphConv(in_size, hidden_size, norm="none", weight=True) for etype in graph.etypes},
-            aggregate="mean",
+        self.conv_layers = ModuleList(
+            [
+                HeteroGraphConv(
+                    {
+                        etype: GraphConv(in_size, in_size, norm="right", weight=True, activation=F.relu)
+                        for etype in graph.etypes
+                    },
+                    aggregate="mean",
+                )
+                for _ in range(BALL_CONV_LAYERS)
+            ]
         )
-        self.layer2 = dglnn.HeteroGraphConv(
-            {etype: dglnn.GraphConv(hidden_size, out_size, norm="none", weight=True) for etype in graph.etypes},
-            aggregate="mean",
-        )
+        sizes = [in_size, hidden_size, out_size]
+        self.class_layers = ModuleList([Linear(i, o) for i, o in zip(sizes, sizes[1:])])
 
-    def forward(self, graph, feature="blocks"):
-        h_dict = self.layer1(graph, {"chunk": graph.ndata[feature]})
-        h_dict = self.layer2(graph, h_dict)
-        slice_classes = F.softmax(h_dict["chunk"])
-        return slice_classes
+    def forward(self, graph):
+        h_dict = {"chunk": graph.ndata["blocks"]}
+        for layer in self.conv_layers:
+            h_dict = layer(graph, h_dict)
+        result = h_dict["chunk"]
+        for layer in self.class_layers:
+            result = layer(result)
+        return result
+
+    def forward_batch(self, blocks):
+        if len(blocks) != len(self.conv_layers):
+            raise ValueError(f"Len of blocks {len(blocks)} not matching num of conv layers: {len(self.conv_layers)}")
+        h_dict = {"chunk": blocks[0].srcdata["blocks"]}
+        for layer, blocks in zip(self.conv_layers, blocks):
+            h_dict = layer(blocks, h_dict)
+        result = h_dict["chunk"]
+        for layer in self.class_layers:
+            result = layer(result)
+        return result
