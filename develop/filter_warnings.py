b1f4646c5c180c3bdf08b6193026ecbe3fdd4a1d
Niklas Beierl <beierl09@ads.uni-passau.de>
2021-06-09 16:04:56 +0200

    Update: Explanations for warning filters.

--- develop/filter_warnings.py
+++ develop/filter_warnings.py
@@ -1,4 +1,10 @@
 import warnings
 
+# A bunch of warnings that clutter my logs, maybe I should start using loguru
+
+# https://github.com/dmlc/dgl/pull/2902
 warnings.filterwarnings("ignore", message="DGLGraph\.__len__")
+# See readme
 warnings.filterwarnings("ignore", message="Undefined\ type\ encountered")
+# Uncomment this whenever you change something in the symbol encoder!
+warnings.filterwarnings("ignore", message="Misaligned\ pointer\ detected")
