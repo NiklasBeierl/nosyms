9c991ab2fd3075d384544bf5ec2efae113c4aeaf
Niklas Beierl <beierl09@ads.uni-passau.de>
2021-09-15 23:55:46 +0200

    Update: Analze_paging_data
    Get confusion matrix for predictions.

--- paging_detection/analyze_paging_data.py
+++ paging_detection/analyze_paging_data.py
@@ -1,34 +1,98 @@
+from collections import defaultdict, Counter
+from functools import cache
 import json
+from typing import Dict
 
-import networkx as nx
 import pandas as pd
 
-from paging_detection import Snapshot, PageTypes
+from paging_detection import Snapshot, PageTypes, PagingStructure
+
+_PAGE_TYPES_ORDERED = tuple(PageTypes)
 
 
-def get_node_features(graph: nx.DiGraph) -> pd.DataFrame:
-    """
-    Create a pandas dataframe with some useful stats for every node in a paging structures graph.
-    """
-    df = pd.DataFrame(data=[graph.nodes[node] for node in graph.nodes], index=graph.nodes)
-    idx, deg = tuple(zip(*graph.in_degree))
-    df["in_degree"] = pd.Series(data=deg, index=idx)
-    idx, deg = tuple(zip(*graph.out_degree))
-    # Note: Out degree will be 0 for page tables in "training" data and >0 in "target" data
-    df["out_degree"] = pd.Series(data=deg, index=idx)
-    return df
+class CachedMapsToData:
+    def __init__(self, pages: Dict[int, PagingStructure]):
+        self.pages = pages
+
+    @cache
+    def __call__(self, page: int, page_type: PageTypes) -> bool:
+        """
+        Determine if a page under a certain designation ends up mapping to any data pages.
+        """
+
+        page = self.pages[page]
+
+        assert page_type in page.designations
+
+        if page_type == PageTypes.PT and page.entries:
+            return True
+
+        for entry in page.entries.values():
+            if entry.target_is_physical(page_type):
+                return True
+            else:
+                return self(entry.target, _PAGE_TYPES_ORDERED[_PAGE_TYPES_ORDERED.index(page_type) + 1])
+        return False
+
+
+def calculate_errors(pages_truth, pages_predicted):
+    maps_to_data = CachedMapsToData(pages_truth)
+    true_positives = defaultdict(lambda: 0)
+    false_positives = defaultdict(lambda: 0)
+    true_negatives = defaultdict(lambda: 0)
+    false_negatives = defaultdict(lambda: 0)
+    # Considering false negatives which did not map to any data
+    false_negatives_with_empty = defaultdict(lambda: 0)
+
+    for offset, pred in pages_predicted.items():
+        true_designations = pages_truth[offset].designations if offset in pages_truth else set()
+
+        for page_type in true_designations & pred.designations:
+            true_positives[page_type] += 1
+        for page_type in set(PageTypes) - (true_designations | pred.designations):
+            true_negatives[page_type] += 1
+        for page_type in pred.designations - true_designations:
+            false_positives[page_type] += 1
+        for page_type in true_designations - pred.designations:
+            false_negatives_with_empty[page_type] += 1
+            if maps_to_data(offset, page_type):
+                false_negatives[page_type] += 1
+    return true_positives, false_positives, true_negatives, false_negatives, false_negatives_with_empty
 
 
 if __name__ == "__main__":
-    with open("../data_dump/all_pages.json") as f:
-        snapshot = Snapshot.validate(json.load(f))
+    import argparse
+    import pathlib
 
-    pages = snapshot.pages
+    parser = argparse.ArgumentParser()
+    parser.add_argument("predictions", help="Snapshot JSON with predicted designations.", type=pathlib.Path)
+    parser.add_argument("truths", help="Snapshot JSON with true paging structures.", type=pathlib.Path)
+    args = parser.parse_args()
 
-    graph = nx.read_graphml("../data_dump/all_pages.graphml")
-    data_pages = [node for node, data in graph.nodes.items() if not data[str(PageTypes.PT)]]
-    graph.remove_nodes_from(data_pages)
+    print("Loading page data.")
+    with open(args.predictions) as f:
+        predicted = Snapshot.validate(json.load(f))
 
-    node_data = get_node_features(graph)
+    with open(args.truths) as f:
+        truth = Snapshot.validate(json.load(f))
+
+    print("Filtering out of bound entries.")
+    for page in truth.pages.values():
+        page.entries = {offset: entry for offset, entry in page.entries.items() if entry.target < truth.size}
+
+    print("Counting errors.")
+    tp, fp, tn, fn, fnwe = calculate_errors(truth.pages, predicted.pages)
+
+    truth_counts = Counter((page_type for page in truth.pages.values() for page_type in page.designations))
+
+    summary = [(pt, tp[pt], fp[pt], tn[pt], fn[pt], fnwe[pt]) for pt in PageTypes]
+    summary_df = pd.DataFrame(summary, columns=["Type", "TP", "FP", "TN", "FN", "FN (with empty)"]).set_index("Type")
+    summary_df["true counts"] = pd.Series(truth_counts)
+
+    total = len(predicted.pages)
+    summary_df["accuracy"] = (summary_df["TP"] + summary_df["TN"]) / total
+    summary_df["recall"] = summary_df["TP"] / summary_df["true counts"]
+    summary_df["precision"] = summary_df["TP"] / (summary_df["TP"] + summary_df["FP"])
+    print(summary_df.to_string())
 
 print("Done")
